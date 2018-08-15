a087a4237407f579dcd7ad20b4a47f493d5343ef
LaeLaeWin <emolay491997@gmail.com>
2018-08-14 22:21:23 -0700

    add new route

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/img1.html')
+def img1():
+    return render_template('img1.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
