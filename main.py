062a9eaad8507e2650dfa8419c58f2d507e545d3
LaeLaeWin <emolay491997@gmail.com>
2018-08-14 23:12:03 -0700

    add file

--- main.py
+++ main.py
@@ -11,5 +11,49 @@ def index():
 def img1():
     return render_template('img1.html')
 
+@app.route('/img2.html')
+def img1():
+    return render_template('img2.html')
+
+@app.route('/img3.html')
+def img1():
+    return render_template('img3.html')
+
+@app.route('/img4.html')
+def img1():
+    return render_template('img4.html')
+
+@app.route('/img5.html')
+def img1():
+    return render_template('img5.html')
+
+@app.route('/img6.html')
+def img1():
+    return render_template('img6.html')
+
+@app.route('/img7.html')
+def img1():
+    return render_template('img7.html')
+
+@app.route('/img8.html')
+def img1():
+    return render_template('img8.html')
+
+@app.route('/b.html')
+def img1():
+    return render_template('b.html')
+
+@app.route('/delivery.html')
+def img1():
+    return render_template('delivery.html')
+
+@app.route('/s2.html')
+def img1():
+    return render_template('s2.html')
+
+@app.route('/sale.html')
+def img1():
+    return render_template('sale.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
